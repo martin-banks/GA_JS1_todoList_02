fe78416c16881644bc10d7b0adb639fb7dc374bc
Martin Banks <anotherbanksy@mac.com>
2016-06-20 21:22:02 +1000

    creating notes. moving note to arrchive but only first time

--- main.js
+++ main.js
@@ -19,13 +19,93 @@ Here are some bonus tasks to push your DOM knowledge!
   - Instead of deleting it completely, move it to a second list called "Archive"
 
 */
+// delegate function from remote file
+function siblings(selector) {
+  var element = document.querySelector(selector)
+  var childElements = Array.from(element.parentNode.children)
+  return childElements.filter(function(child) {
+    return child !== element
+  })
+}
+
+function closest(element, query) {
+  while (element !== document) {
+    if (element.matches(query)) {
+      return element
+    }
+    element = element.parentNode
+  }
+  return null
+}
+
+function delegate(selector, eventName, targetSelector, listener) {
+  document.querySelector(selector).addEventListener(eventName, function (event) {
+    var closestMatch = closest(event.target, targetSelector)
+    if (closestMatch) {
+      event.delegateTarget = closestMatch
+      listener(event)
+    }
+  })
+}
+
+
 
 (function() {
 
-  var container = document.querySelector('#container')
-  var state = {}
+ 	var container = document.querySelector('#container');
+ 	var state = {};
 
-  function render(data, into) {
+ 	var archiveTemplate = '<h2>Archive</h2><ul id="archive-list"></ul>'
 
-  }
+ 	var listTemplate = function(param){
+ 		return '<li class="list-thing">' + param + '</li>'
+ 	}
+
+
+
+
+
+ 	delegate('#my-list', 'click', 'li', function(event){
+ 		console.log(event.target.textContent);
+ 		event.target.remove();
+ 		
+
+ 		if ( !document.querySelector('#archive-list') ) {
+ 			document.querySelector('#container').innerHTML += archiveTemplate;
+ 		};
+
+ 		document.getElementById('archive-list').innerHTML+=listTemplate(event.target.textContent);
+
+ 
+ 	});
+
+
+
+
+
+
+ 	document.getElementById('new-thing-button').addEventListener('click', function(event){
+ 		event.preventDefault();
+ 		var newText = document.getElementById('new-thing').value;
+
+ 		if(newText.length >0){
+ 			document.getElementById('my-list').innerHTML += listTemplate(newText);
+ 		}
+
+ 		document.getElementById('new-thing').value = '';
+ 	});
+
+
+ 	
+
+
+
+
+
+
+
+ 	function render(data, into) {
+ 	}
+
+
 })()
