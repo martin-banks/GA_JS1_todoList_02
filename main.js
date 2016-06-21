71b763d9d98fc4b6da41e25233e37d8a0f107e19
Martin Banks <anotherbanksy@gmail.com>
2016-06-21 12:03:42 +1000

    change button to use delegate function. still have bug where button refreshes after item moved to archove

--- main.js
+++ main.js
@@ -14,78 +14,96 @@ Your code must use these features:
 
 Here are some bonus tasks to push your DOM knowledge!
 - Bonus tasks:
-  - When a list item is archived, change its background colour to "green"
-  - Add a link to each item to delete it completely
-  - Instead of deleting it completely, move it to a second list called "Archive"
+	- When a list item is archived, change its background colour to "green"
+	- Add a link to each item to delete it completely
+	- Instead of deleting it completely, move it to a second list called "Archive"
 
 */
 // delegate function from remote file
+
+// not in use for this demo
 function siblings(selector) {
-  var element = document.querySelector(selector)
-  var childElements = Array.from(element.parentNode.children)
-  return childElements.filter(function(child) {
-    return child !== element
-  })
+	var element = document.querySelector(selector)
+	var childElements = Array.from(element.parentNode.children)
+	return childElements.filter(function(child) {
+		return child !== element
+	})
 }
 
+/* --------------------- */
+
+
+// element = DOM element interacted with
+// query = DOM element we're looking for
 function closest(element, query) {
-  while (element !== document) {
-    if (element.matches(query)) {
-      return element
-    }
-    element = element.parentNode
-  }
-  return null
+	while (element !== document) {
+		if (element.matches(query)) {
+			return element
+		}
+		// element operating on moves up DOM tree
+		element = element.parentNode
+	}
+	return null
 }
 
+// selctor = parent DOM element
+// eventName = 'click' / user action
+// targetSelector: child element of slector to run function(listener) on
+// listener: function to call
+
+// create function called delegate 
 function delegate(selector, eventName, targetSelector, listener) {
-  document.querySelector(selector).addEventListener(eventName, function (event) {
-    var closestMatch = closest(event.target, targetSelector)
-    if (closestMatch) {
-      event.delegateTarget = closestMatch
-      listener(event)
-    }
-  })
+	// get DOM element(selector) add event listener (user action, declare function)
+	document.querySelector(selector).addEventListener(eventName, function (event) {
+		// call closest 'function' (above) where
+		// element = event.target
+		// query = targetSelector
+		var closestMatch = closest(event.target, targetSelector)
+		if (closestMatch) {
+			event.delegateTarget = closestMatch
+			listener(event)
+		}
+	})
 }
 
 
 
 (function() {
 	var state = {};
- 	var $container = document.querySelector('#container');
- 	var $archiveList = function() {
- 		return document.querySelector('#archive-list');
- 	}
- 	var archiveTemplate = '<h2>Archive</h2><ul id="archive-list"></ul>';
- 	var $newThing = document.getElementById('new-thing');
- 	var $myList = document.getElementById('my-list')
+	var $container = document.querySelector('#container');
+	var $archiveList = function() {
+		return document.querySelector('#archive-list');
+	}
+	var archiveTemplate = '<h2>Archive</h2><ul id="archive-list"></ul>';
+	var $newThing = document.getElementById('new-thing');
+	var $myList = document.getElementById('my-list')
 
- 	console.log($archiveList())
- 	delegate('#container', 'click', 'li.list-thing', function(event){
- 		console.log(event.target.textContent);
- 		event.target.remove();
- 		if ( !$archiveList() ) {
- 			$container.innerHTML += archiveTemplate;
- 		};
- 		$archiveList().innerHTML += `<li class="archive-thing">${event.target.textContent}</li>`
-
- 	});
-
+	delegate('#container', 'click', 'li.list-thing', function(event){
+		console.log(event.target.textContent);
+		event.target.remove();
+		if ( !$archiveList() ) {
+			$container.innerHTML += archiveTemplate;
+		};
+		$archiveList().innerHTML += `<li class="archive-thing">${event.target.textContent}</li>`;
+		return null;
+	});
 
 
- 	document.getElementById('new-thing-button').addEventListener('click', function(event){
- 		event.preventDefault();
- 		render($newThing.value, $myList)
- 	});
+	delegate('form', 'click', '#new-thing-button', function(event){
+		event.preventDefault();
+		render($newThing.value, $myList);
+	})
 
 
-
- 	function render(data, into) {
- 		event.preventDefault();
-		into.innerHTML += `<li class="list-thing">${data}</li>`;
- 		
- 		$newThing.value = '';
- 	}
+	function render(data, into) {
+		event.preventDefault();
+		if(data.length>0){
+			into.innerHTML += `<li class="list-thing">${data}</li>`;
+		};
+		
+		$newThing.value = '';
+		return null;
+	}
 
 
 })()
