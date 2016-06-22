b96bae8a004981988108f52d1122e29759fbd41e
Martin Banks <anotherbanksy@gmail.com>
2016-06-22 13:22:33 +1000

    reinstate utitlity function in html. remove uneccessary helper function. revise notes.

--- main.js
+++ main.js
@@ -1,45 +1,42 @@
 
 (function() {
 	var state = {};
-	var $archiveList = function() {
-		return document.querySelector('#archive-list');
-	};
-
+	
 	function updateList(data, removeThis){
-		var listTemplate = `<li>${data.text}</li>`;
+		var listTemplate = `<li>${data.text}</li>`; //es6 template
 		if (data.placeIn === 'todo'){ // put in todo list
 			document.getElementById('my-list').innerHTML += listTemplate; // better alternative to += ?
 			document.getElementById('new-thing').value = '';	
-		} ;
+		}
 		else if (data.placeIn === 'archive'){ // put in archive
 			document.getElementById('archive-list').innerHTML += listTemplate; 
-			removeThis.remove();
+			removeThis.remove(); // remove li clicked on from DOM
 		};
-		state.placeIn = '';
+		state.placeIn = ''; // reset values for object keys
 		state.text = '';
 	};
 
 	// delegate for clicking on li in todo list - creates and moves to archive
 	delegate('#my-list', 'click', 'li', function(event){
-		console.log('event', event.target);
-		console.log('delegate.target', event.delegateTarget);
-		console.log('parentNode', event.target.parentNode);
+		console.log('event', event.target); // what was clicked
+		console.log('delegate.target', event.delegateTarget); // what was clicked -  from delegate function
+		console.log('parentNode', event.target.parentNode); // clicked item parent
 
-		if ( !document.getElementById('archive-head') ) {
-			var arcHead = document.createElement('h2');
+		if ( !document.getElementById('archive-head') ) { // if there is no 'archive'
+			var arcHead = document.createElement('h2'); // create one
 			arcHead.id = 'archive-head';
 			arcHead.textContent = 'Archive';
-			document.getElementById('container').appendChild(arcHead);
+			document.getElementById('container').appendChild(arcHead); // append to #container
 
-			var archiveList = document.createElement('ul');
+			var archiveList = document.createElement('ul'); // create ul for archive
 			archiveList.id = 'archive-list';
-			document.getElementById('container').appendChild(archiveList)
+			document.getElementById('container').appendChild(archiveList) 
 		};
-		state.text = event.target.textContent;
-		state.placeIn = 'archive';
-		updateList(state, event.target);	
-		// stop bubble phase as event.target has been removed from nodeList 
-		event.stopPropagation(); 
+		state.text = event.target.textContent; // set text in state object
+		state.placeIn = 'archive'; // set list to put todo in 
+		updateList(state, event.target);//stop bubble phase as event.target has been removed from nodeList 
+		
+		event.stopPropagation();
 		return false;
 	});
 
@@ -51,9 +48,9 @@
 			state.text = document.getElementById('new-thing').value;
 			updateList(state);
 		};
-	})
-
-})()
+	});
+	
+})();
 
 
 
